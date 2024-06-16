5fef68a06d8ac4385c5a75408b0ba822e7640e32
Victor Andrade <victorsouzaa.dev@gmail.com>
2024-06-16 01:56:13 -0300

    Criando o projeto de operadores java

---
+++ operadores-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+
+    public static void main (String[]args) {
+        String nomeUm = "Victor";
+        String nomeDois = new String("Victor");
+        
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 < numero2) {
+            System.out.println("a nossa condição é verdadeira");
+            
+        }
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois?" + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeroDois?" + simNao);
+
+    }
+}
